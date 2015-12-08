d1bd1e8924a09d9a4b7d42f5e0213f2d03d77feb
Дмитрий Седых <sedykh@gmail.com>
2015-12-08 20:38:06 +0300

    test fix

--- nats-geotrack/main_test.go
+++ nats-geotrack/main_test.go
@@ -49,10 +49,10 @@ func TestSubscription(t *testing.T) {
 	)
 	{
 		fmt.Println("LBS Request")
-		var data *geo.Point
+		var data = new(geo.Point)
 		err = nce.Request(serviceNameLBS,
 			`864078-35827-010003698-fa-2-1e50-772a-95-1e50-773c-a6-1e50-7728-a1-1e50-7725-92-1e50-772d-90-1e50-7741-90-1e50-7726-88`,
-			&data, timemout)
+			data, timemout)
 		if err != nil {
 			t.Fatal(err)
 		}
